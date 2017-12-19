0739f5381a69f13d530161827e6eaa5d2947464f
anonymouswebprogrammer <b.a.peters@student.tue.nl>
2017-12-19 13:48:34 +0100

    Added views for edit

--- list/urls.py
+++ list/urls.py
@@ -6,4 +6,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', views.todo_list, name='todo_list'),
     url(r'^todo/(?P<pk>\d+)/$', views.todo_detail, name='todo_detail'),
+    url(r'^todo/new/$', views.todo_new, name='todo_new'),
+    url(r'^todo/(?P<pk>\d+)/edit/$', views.todo_edit, name='todo_edit'),
 ]
